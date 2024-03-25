782f74d42ea771036b8346436ecbf52017649a2a
Nik <kodjima33@gmail.com>
2024-03-24 20:47:52 -0700

    adding all apps

---
+++ AppWithWearable/lib/backend/supabase/supabase.dart
@@ -0,0 +1,7 @@
+import 'package:supabase_flutter/supabase_flutter.dart' hide Provider;
+
+export 'database/database.dart';
+
+const _kSupabaseUrl = 'https://zimavzhsuyykrvtgxghi.supabase.co';
+const _kSupabaseAnonKey =
+    '***REMOVED_TWILIO_TOKEN***
