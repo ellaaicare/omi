43955d45d2419c83056266cbf90d55be5e437fc6
Nik <kodjima33@gmail.com>
2024-04-01 15:14:15 -0700

    Updates to app

--- AppWithWearable/lib/backend/supabase/supabase.dart
+++
@@ -1,7 +0,0 @@
-import 'package:supabase_flutter/supabase_flutter.dart' hide Provider;
-
-export 'database/database.dart';
-
-const _kSupabaseUrl = 'https://zimavzhsuyykrvtgxghi.supabase.co';
-const _kSupabaseAnonKey =
-    '***REMOVED_TWILIO_TOKEN***
